071f0649e1c42297bea523cbe404d792c6ca4169
Ismail Hossain <hossain.eee042@gmail.com>
2023-09-08 14:53:35 +0600

    make search bar out of scroll

--- lib/modules/screens/home screen/view/home_screen.dart
+++ lib/modules/screens/home screen/view/home_screen.dart
@@ -15,126 +15,102 @@ class HomeScreen extends StatelessWidget {
     var size = MediaQuery.sizeOf(context);
     return Scaffold(
       body: SafeArea(
-          child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-              horizontal: size.width * 0.04, vertical: size.height * 0.04),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              //srarch bar, shopping cart and bell icon
-              SearchProduct(size: size),
-              SizedBox(
-                height: size.height * 0.04,
-              ),
-              //cashback offer card
-              CashBackOffer(size: size),
-              //height space
-              SizedBox(
-                height: size.height * 0.04,
-              ),
-              //widget for several option
-              SeveralOptionWithIcon(
-                size: size,
-              ),
-              //height space
-              SizedBox(
-                height: size.height * 0.04,
-              ),
-              Row(
-                children: [
-                  Text(
-                    "Special for you",
-                    style: TextStyle(fontSize: size.width * 0.04),
-                  ),
-                  Spacer(),
-                  Text(
-                    "See More",
-                    style: TextStyle(
-                        color: Colors.grey, fontSize: size.width * 0.035),
-                  ),
-                ],
-              ),
-              //height space
-              SizedBox(
-                height: size.height * 0.04,
-              ),
-
-              //Special offer for you
-              SizedBox(
-                height: size.height * 0.15,
-                child: SpecialOfferImage(
-                  size: size,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: size.width * 0.04, vertical: size.height * 0.04),
+            child: Column(
+              children: [
+                  //srarch bar, shopping cart and bell icon
+                SearchProduct(size: size),
+                SizedBox(
+                  height: size.height * 0.04,
                 ),
-              ),
-              //height space
-              SizedBox(
-                height: size.height * 0.04,
-              ),
-              Row(
-                children: [
-                  Text(
-                    "Popular Product",
-                    style: TextStyle(fontSize: size.width * 0.04),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                      /*   //srarch bar, shopping cart and bell icon
+                        SearchProduct(size: size),
+                        SizedBox(
+                          height: size.height * 0.04,
+                        ), */
+                        //cashback offer card
+                        CashBackOffer(size: size),
+                        //height space
+                        SizedBox(
+                          height: size.height * 0.04,
+                        ),
+                        //widget for several option
+                        SeveralOptionWithIcon(
+                          size: size,
+                        ),
+                        //height space
+                        SizedBox(
+                          height: size.height * 0.04,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Special for you",
+                              style: TextStyle(fontSize: size.width * 0.04),
+                            ),
+                            Spacer(),
+                            Text(
+                              "See More",
+                              style: TextStyle(
+                                  color: Colors.grey, fontSize: size.width * 0.035),
+                            ),
+                          ],
+                        ),
+                        //height space
+                        SizedBox(
+                          height: size.height * 0.04,
+                        ),
+                                  
+                        //Special offer for you
+                        SizedBox(
+                          height: size.height * 0.15,
+                          child: SpecialOfferImage(
+                            size: size,
+                          ),
+                        ),
+                        //height space
+                        SizedBox(
+                          height: size.height * 0.04,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Popular Product",
+                              style: TextStyle(fontSize: size.width * 0.04),
+                            ),
+                            Text(
+                              "See More",
+                              style: TextStyle(
+                                  color: Colors.grey, fontSize: size.width * 0.035),
+                            ),
+                          ],
+                        ),
+                        //height space
+                        SizedBox(
+                          height: size.height * 0.04,
+                        ),
+                        //popular product image
+                        SizedBox(
+                          height: size.height * 0.2,
+                          child: PopularProduct(
+                            size: size,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                  Spacer(),
-                  Text(
-                    "See More",
-                    style: TextStyle(
-                        color: Colors.grey, fontSize: size.width * 0.035),
-                  ),
-                ],
-              ),
-              //height space
-              SizedBox(
-                height: size.height * 0.04,
-              ),
-              //popular product image
-              SizedBox(
-                height: size.height * 0.2,
-                child: PopularProduct(
-                      size: size,
-                    
-                    ),
-                /* child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    PopularProduct(
-                      size: size,
-                      image: ImagePath.mobile,
-                      onTap: () {
-                        print("Click on First popular Product");
-                      },
-                    ),
-                    PopularProduct(
-                      size: size,
-                      image: ImagePath.mobile,
-                      onTap: () {
-                        print("Click on Second popular Product");
-                      },
-                    ),
-                    PopularProduct(
-                      size: size,
-                      image: ImagePath.mobile,
-                      onTap: () {
-                        print("Click on Third popular Product");
-                      },
-                    ),
-                    PopularProduct(
-                      size: size,
-                      image: ImagePath.mobile,
-                      onTap: () {
-                        print("Click on Fourth popular Product");
-                      },
-                    ),
-                  ],
-                ), */
-              )
-            ],
-          ),
-        ),
-      )),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
