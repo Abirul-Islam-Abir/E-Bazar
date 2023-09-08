2b6a530ffc298fce43e4f82d14e0c35baad8a265
Ismail Hossain <hossain.eee042@gmail.com>
2023-09-08 13:44:24 +0600

    remove unnecessary comment code

--- lib/modules/widget/home_screen_widget/several_option_with_icon.dart
+++ lib/modules/widget/home_screen_widget/several_option_with_icon.dart
@@ -7,15 +7,9 @@ class SeveralOptionWithIcon extends StatelessWidget {
   SeveralOptionWithIcon({
     super.key,
     required this.size,
-    /*    required this.iconData,
-    required this.text,
-    required this.onTap, */
   });
 
   final Size size;
-  /*  final IconData iconData;
-  final String text;
-  final Function onTap; */
 
   final List<HomeScreenModel> iconListForSeveralOption = [
     HomeScreenModel.icons(
@@ -36,12 +30,7 @@ class SeveralOptionWithIcon extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    /*  width: size.width * 0.16,
-      height: size.height * 0.09,
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 250, 232, 220),
-        borderRadius: BorderRadius.circular(7),
-      ), */
+ 
     return SizedBox(
       width: double.infinity,
       height: size.height * 0.09,
@@ -78,39 +67,6 @@ class SeveralOptionWithIcon extends StatelessWidget {
             );
           }),
     );
-    /* Column(
-      children: [
-        Container(
-            width: size.width * 0.16,
-            height: size.height * 0.09,
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 250, 232, 220),
-              borderRadius: BorderRadius.circular(7),
-            ),
-            child: IconButton(
-              onPressed: () {
-                onTap(); //take action when user click on icon
-              },
-              icon: Icon(
-                iconData,
-                size: size.width * 0.07,
-                color: const Color.fromARGB(255, 250, 182, 158),
-              ),
-            )),
-        SizedBox(
-          height: size.height * 0.009,
-        ),
-        SizedBox(
-          width: size.width * 0.1,
-          height: size.height * 0.05,
-          child: Text(
-            text,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-                fontSize: 11, color: Color.fromARGB(255, 134, 133, 133)),
-          ),
-        ),
-      ],
-    ); */
+
   }
 }
